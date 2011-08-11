b28e24bfd8cd0991935df934e824a2ab9924b4cb
KUOKA Yusuke <ykuoka@gmail.com>
2011-08-12 01:33:34 +0900

    Syntax highlight for Play Template's tag names, and embedded HTML/Groovy highlighting with each native highlighter.

--- src/mumoshu/idea/plugins/play/highlighter/PlayTemplateEditorHighlighter.java
+++ src/mumoshu/idea/plugins/play/highlighter/PlayTemplateEditorHighlighter.java
@@ -1,16 +1,23 @@
 package mumoshu.idea.plugins.play.highlighter;
 
-import com.intellij.ide.highlighter.HtmlFileHighlighter;
+import com.intellij.lang.Language;
+import com.intellij.lang.StdLanguages;
+import com.intellij.openapi.editor.JspHighlighterColors;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.ex.util.LayerDescriptor;
 import com.intellij.openapi.editor.ex.util.LayeredLexerEditorHighlighter;
 import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import mumoshu.idea.plugins.play.template.lexer.PlayTemplateTokenTypes;
+import mumoshu.idea.plugins.play.template.psi.impl.PlayTemplateFileImpl;
 import org.jetbrains.plugins.groovy.highlighter.GroovySyntaxHighlighter;
-
-import javax.swing.text.DefaultHighlighter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,22 +28,75 @@ import javax.swing.text.DefaultHighlighter;
  */
 public class PlayTemplateEditorHighlighter extends LayeredLexerEditorHighlighter {
 
+    private Language myTemplateLanguage = null;
+
+    private final Project myProject;
+    private final VirtualFile myFile;
+
     public PlayTemplateEditorHighlighter(EditorColorsScheme scheme, Project project, VirtualFile virtualFile) {
         super(new PlayTemplateSyntaxHighlighter(), scheme);
-        registerHtmlHighlighter();
+
+        myProject = project;
+        myFile = virtualFile;
+
+        // HTML layer
+//        SyntaxHighlighter highlighter = new HtmlFileHighlighter();
+        SyntaxHighlighter highlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(StdLanguages.HTML, project, virtualFile);
+        LayerDescriptor descriptor = new LayerDescriptor(highlighter, "\n");
+        registerLayer(PlayTemplateTokenTypes.TEXT, descriptor);
+
+        // Groovy layer
         registerGroovyHighlighter();
     }
 
     private void registerGroovyHighlighter() {
         SyntaxHighlighter highlighter = new GroovySyntaxHighlighter();
-        LayerDescriptor descriptor = new LayerDescriptor(highlighter, "\n");
+        LayerDescriptor descriptor = new LayerDescriptor(highlighter, "\n", JspHighlighterColors.JSP_SCRIPTING_BACKGROUND);
         registerLayer(PlayTemplateTokenTypes.SCRIPT_BODY, descriptor);
     }
 
-    private void registerHtmlHighlighter() {
-        SyntaxHighlighter highlighter = new HtmlFileHighlighter();
-        LayerDescriptor descriptor = new LayerDescriptor(highlighter, "\n");
-        registerLayer(PlayTemplateTokenTypes.TEXT, descriptor);
+    @Override
+    protected boolean updateLayers() {
+        Language templateLanguage = getCurrentTemplateLanguage();
+        if (!Comparing.equal(myTemplateLanguage, templateLanguage)) {
+//            unregisterLayer(PlayTemplateTokenTypes.TEXT);
+//            myTemplateLanguage = templateLanguage;
+//            com.intellij.openapi.fileTypes.SyntaxHighlighter templateLanguageHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(myTemplateLanguage, myProject, myFile);
+//            registerLayer(PlayTemplateTokenTypes.TEXT, new LayerDescriptor(templateLanguageHighlighter, "", null));
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    public static PlayTemplateFileImpl getPlayTemplateFile(PsiElement element) {
+        if (element == null)
+            return null;
+        PsiFile containingFile = element.getContainingFile();
+        if (containingFile == null) {
+            return null;
+        } else {
+            FileViewProvider viewProvider = containingFile.getViewProvider();
+            PsiFile psiFile = viewProvider.getPsi(viewProvider.getBaseLanguage());
+            return (psiFile instanceof PlayTemplateFileImpl) ? (PlayTemplateFileImpl)psiFile : null;
+        }
+    }
+
+    private boolean isProjectPresent() {
+        return myProject != null && !myProject.isDisposed() && getDocument() != null;
+    }
+
+    PlayTemplateFileImpl getCurrentPlayTemplateFile() {
+        PsiDocumentManager instance = PsiDocumentManager.getInstance(myProject);
+        PsiFile psiFile = instance.getPsiFile(getDocument());
+        PlayTemplateFileImpl playTemplateFile = getPlayTemplateFile(psiFile);
+        return playTemplateFile;
+    }
+
+    private Language getCurrentTemplateLanguage() {
+        if (isProjectPresent() && getCurrentPlayTemplateFile() != null) {
+            return StdLanguages.HTML;
+        }
+        return null;
+    }
 }
